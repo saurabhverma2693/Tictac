168cedd7c5f204885c11f586aaf4ac8a1d8788ef
saurabh <saurabhverma2693@gmail.com>
2025-07-22 17:39:28 +0530

    Add Players Nmae  Statically

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-
 function App() {
-  
-
   return (
-    <div></div>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
