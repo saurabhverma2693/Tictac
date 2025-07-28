4046fa729be0a3c97c558ab30aaff1d1fc07f356
saurabh <saurabhverma2693@gmail.com>
2025-07-28 15:23:41 +0530

    Update state & Conditional Content

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -3,22 +3,25 @@ export default function Player({name,symbol}){
    const [isEditing, setIsEditing] =  useState(false);
 
    function handleEditClick(){
-    setIsEditing(true);
+    setIsEditing(!isEditing);
    }
 
    let playerName = <span className="player-name">{name}</span>
-   
+   let btnCaption = 'Edit';
+
    if(isEditing){
-    playerName = <input type="text" required />
+    playerName = <input type="text" required value={name}/>
+    btnCaption='Save';
    }
 
     return (
         <li>
-            <span id="players">       
+            <span className="player">       
                 {playerName}     
             <span className="player-symbol">{symbol}</span>
             </span>
-            <button onClick={handleEditClick}>Edit</button>
+            {/* <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button> */}
+            <button onClick={handleEditClick}>{btnCaption}</button>
           </li>
     );
 } 
