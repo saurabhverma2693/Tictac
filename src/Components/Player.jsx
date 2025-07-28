be1a6fc6d70a3b6b3276369a9e63545226572dc5
saurabh <saurabhverma2693@gmail.com>
2025-07-28 14:59:57 +0530

    Concept Repitition: use of state

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,10 +1,24 @@
-export default function Player({player,symbol}){
+import { useState } from "react";
+export default function Player({name,symbol}){
+   const [isEditing, setIsEditing] =  useState(false);
+
+   function handleEditClick(){
+    setIsEditing(true);
+   }
+
+   let playerName = <span className="player-name">{name}</span>
+   
+   if(isEditing){
+    playerName = <input type="text" required />
+   }
+
     return (
         <li>
-            <span id="players">            <span className="player-name">{player}</span>
+            <span id="players">       
+                {playerName}     
             <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>Edit</button>
           </li>
     );
-}
+} 
