a3a2c77da739d6f45e9f861e66e482e949dd800f
saurabh <saurabhverma2693@gmail.com>
2025-08-01 14:19:57 +0530

    Lifting the State Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import Player from "./Components/Player";
 import GmaeBoard from "./Components/GameBoard";
+import { useState } from "react";
 
 function App() {
+  const [activePlayer, SetActivePlayer]  = useState('X');
+
+  function handleSelectPlayer(){
+    SetActivePlayer((curActivePlayer)=> curActivePlayer === 'X' ? 'O' : 'X');
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player initialName="Player 1" symbol="X"  isActive={activePlayer === 'X'}/>
+          <Player initialName="Player 2" symbol="O"  isActive={activePlayer === 'O'}/>
         </ol>
-        <GmaeBoard />
+        <GmaeBoard onSelectSquare={handleSelectPlayer}  activePlayerSymbol={activePlayer}/>
       </div>
+      LOG
     </main>
   );
 }
